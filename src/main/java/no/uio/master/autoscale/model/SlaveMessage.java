032e96a9b55267eefe5fb32721c61cca869bbf98
Andreas Baakind <toraba@ifi.uio.no>
2012-10-08 12:39:45 +0200

    Added files again

---
+++ src/main/java/no/uio/master/autoscale/model/SlaveMessage.java
@@ -0,0 +1,50 @@
+package no.uio.master.autoscale.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import no.uio.master.autoscale.model.enumerator.SlaveMessageType;
+
+
+public class SlaveMessage implements Serializable {
+	private static final long serialVersionUID = 382125954042009372L;
+
+	
+	private SlaveMessageType type;
+	private HashMap<String, Object> map;
+	
+	public SlaveMessage() {
+		type = SlaveMessageType.INITIALIZATION;
+		map = new HashMap<String, Object>();
+	}
+	
+	public SlaveMessage(SlaveMessageType type) {
+		this.type = type;
+		map = new HashMap<String, Object>();
+	}
+
+	public SlaveMessageType getType() {
+		return type;
+	}
+
+	public void setType(SlaveMessageType type) {
+		this.type = type;
+	}
+
+	public HashMap<String, Object> getMap() {
+		return map;
+	}
+
+	public void setMap(HashMap<String, Object> map) {
+		this.map = map;
+	}
+	
+	public void put(String key, Object value) {
+		map.put(key, value);
+	}
+
+	@Override
+	public String toString() {
+		return "SlaveMessage [type=" + type + ", map=" + map + "]";
+	}
+}
---
+++ src/main/java/no/uio/master/autoscale/model/enumerator/SlaveMessageType.java
@@ -0,0 +1,8 @@
+package no.uio.master.autoscale.model.enumerator;
+
+
+public enum SlaveMessageType {
+	INITIALIZATION,
+	UPDATE,
+	STOP_DAEMON;
+}
